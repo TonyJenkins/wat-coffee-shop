c890f032ef55ca1a18837439af6fad44cd252da5
Tony Jenkins <tony@tony-jenkins.co.uk>
2025-11-11 12:31:52 +0000

    Enhance index page

--- wat_coffee/drinks_counter/views.py
+++ wat_coffee/drinks_counter/views.py
@@ -3,11 +3,16 @@ from django.shortcuts import render, get_object_or_404
 from .models import Drink
 
 def index(request):
-    return render(request, 'drinks_counter/index.html')
+    drinks = Drink.objects.all()
+
+    context = {
+        'drinks': drinks,
+    }
+
+    return render(request, 'drinks_counter/index.html', context)
 
 
 def drink_display(request, slug):
-
     drink = get_object_or_404(Drink, slug=slug)
 
     context = {
@@ -15,5 +20,3 @@ def drink_display(request, slug):
     }
 
     return render(request, 'drinks_counter/drink_display.html', context)
-
-
