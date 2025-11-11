871bb09864ed079da70173f2a45488a586a5888e
Tony Jenkins <tony@tony-jenkins.co.uk>
2025-11-11 11:05:37 +0000

    Register for Admin

--- wat_coffee/drinks_counter/admin.py
+++ wat_coffee/drinks_counter/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Drink
+
+@admin.register(Drink)
+class DrinkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'size', 'price', 'available', 'caffeine_mg', 'created_at', 'updated_at')
+    list_filter = ('size', 'available')
+    search_fields = ('name', 'description', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    readonly_fields = ('created_at', 'updated_at')
+    ordering = ('name',)
