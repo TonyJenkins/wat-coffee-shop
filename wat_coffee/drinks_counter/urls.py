613ebc13d4d465844230f95c495ffc5801fe0fdb
Tony Jenkins <tony@tony-jenkins.co.uk>
2025-11-11 12:24:27 +0000

    Add basic index page

--- wat_coffee/drinks_counter/urls.py
+++ wat_coffee/drinks_counter/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'drinks_counter'
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('<slug:slug>', views.drink_display, name='drink_display'),
 ]
--- wat_coffee/drinks_counter/views.py
+++ wat_coffee/drinks_counter/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Drink
 
+def index(request):
+    return render(request, 'drinks_counter/index.html')
+
 
 def drink_display(request, slug):
 
@@ -12,3 +15,5 @@ def drink_display(request, slug):
     }
 
     return render(request, 'drinks_counter/drink_display.html', context)
+
+
