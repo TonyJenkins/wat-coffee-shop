2860916c957f4466390e8442f2e3a6a026d25c02
Tony Jenkins <tony@tony-jenkins.co.uk>
2025-11-11 10:58:26 +0000

    Migrate and populate

--- wat_coffee/wat_coffee/settings.py
+++ wat_coffee/wat_coffee/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'drinks_counter',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
